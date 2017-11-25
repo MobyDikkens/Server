64378941956b9caddad721eee657eb73602d814b
Victor Petrovich <dimasheludko1999@gmail.com>
2017-11-25 18:15:48 +0200

    MultiThreading: Each client have a personal thread

--- Server/Server/EServer.cs
+++ Server/Server/EServer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Net;
 using System.Net.Sockets;
 using Server.Models;
@@ -61,34 +62,50 @@ namespace Server
         {
             while(true)
             {
-                //Our client
-                TcpClient client = listener.AcceptTcpClient();
-
-                string package = null;
 
                 try
                 {
-                    while (true)
+                    //Our client
+                    TcpClient client = listener.AcceptTcpClient();
+                    Thread thread = null;
+
+                    /*
+                     * string package = null;
+
+                    try
                     {
-                        //stream to work with buffer
-                        NetworkStream networkStream = client.GetStream();
-
-                        //processing a package and adding it to the end of string value
-                        package += Unpackage(networkStream);
-                        
-                        //Check if it is the end of the package
-                        if (package.IndexOf("\0") > -1)
+                        while (true)
                         {
-                            //Find the end of the package
-                            package = package.Substring(0, package.IndexOf("\0"));
+                            //stream to work with buffer
+                            NetworkStream networkStream = client.GetStream();
 
-                            break;
+                            //processing a package and adding it to the end of string value
+                            package += Unpackage(networkStream);
+
+                            //Check if it is the end of the package
+                            if (package.IndexOf("\0") > -1)
+                            {
+                                //Find the end of the package
+                                package = package.Substring(0, package.IndexOf("\0"));
+
+                                break;
+                            }
+
                         }
 
+                        Console.WriteLine(package);
+
+
                     }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                    */
 
-                    Console.WriteLine(package);
+                    thread = new Thread(ClientHandler);
 
+                    thread.Start(client);
                 }
                 catch(Exception ex)
                 {
@@ -98,7 +115,80 @@ namespace Server
             }
         }
 
-        private string Unpackage(NetworkStream networkStream)
+
+        private static void ClientHandler(object o)
+        {
+            TcpClient client = o as TcpClient;
+            string package = "";
+
+
+            if (o == null)
+            {
+                return;
+            }
+            else
+            {
+
+                    while (true)
+                    {
+                        //stream to work with buffer
+                        NetworkStream networkStream = null;
+
+                        try
+                        {
+                            networkStream = client.GetStream();
+                            package += Unpackage(networkStream);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("NetworkStream ERROR");
+                        }
+                        finally
+                        {
+
+                            try
+                            {
+                                client.Close();
+                                networkStream.Close();
+                            }
+                            catch
+                            {
+
+                                Console.WriteLine("Closing ERROR");
+
+                            }
+
+                        }
+
+                        //processing a package and adding it to the end of string value
+
+
+                        //Check if it is the end of the package
+                        if (package.IndexOf("\0") > -1)
+                        {
+                            //Find the end of the package
+                            package = package.Substring(0, package.IndexOf("\0"));
+
+                            break;
+                        }
+
+
+                    }
+
+
+                    Console.WriteLine();
+                    Console.WriteLine("*****************************************************************************");
+                    Console.WriteLine("Recieved data:{0}", package);
+                    //Console.WriteLine("Count of connected clients:{0}", this.Lenth);
+                    Console.WriteLine("*****************************************************************************");
+                    Console.WriteLine();
+
+            }
+        }
+
+
+
+        private static string Unpackage(NetworkStream networkStream)
         {
             //amoun of bytes and package buffer
             int rcvBytes = 65535;
